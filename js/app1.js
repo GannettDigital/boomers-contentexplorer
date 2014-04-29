6cb672e9da962adf861f0b54ce6c993468227d96
Carl Turner <cturner9245@gmail.com>
2014-04-29 18:03:11 -0400

    ala

---
+++ js/app1.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var App = angular.module('RSSFeedApp', []);
+
+App.controller("FeedCtrl", ['$scope', 'FeedService', function ($scope, Feed) {
+    $scope.loadButtonText = "Load";
+    $scope.loadFeed = function (e) {
+        Feed.parseFeed($scope.feedSrc).then(function (res) {
+            $scope.loadButtonText = angular.element(e.target).text();
+            $scope.feeds = res.data.responseData.feed.entries;
+        });
+    }
+}]);
+
+App.factory('FeedService', ['$http', function ($http) {
+    return {
+        parseFeed: function (url) {
+            return $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
+        }
+    }
+}]);
+
+App.config(['$routeProvider',function ($routeProvider) {
+      $routeProvider.
+          when('', {
+              templateUrl: 'views/rss-list.html'});
+        
+  }]);
