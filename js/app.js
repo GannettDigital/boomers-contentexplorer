2f442682ee2f3b9d54486ae11ca9da386134da3d
Carl Turner <cturner9245@gmail.com>
2014-04-29 18:00:47 -0400

    ala

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 /* App Module */
-var mediaApp = angular.module('mediaApp', ['ngResource','ngRoute']);
+var assetApp = angular.module('assetApp', ['ngResource', 'ngRoute']);
 
-mediaApp.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/', {templateUrl: 'views/media-list.html',   controller: 'MediaListCtrl'});
+assetApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/', { templateUrl: 'views/asset-list.html', controller: 'AssetListCtrl' });
 }]);
