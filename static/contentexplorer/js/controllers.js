f4d5cda9fda898860fe0b4820b10045fbb7761a1
Carl Turner <cturner9245@gmail.com>
2014-07-09 08:43:36 -0400

    added pagination and search nowu assets

--- static/contentexplorer/js/controllers.js
+++ static/contentexplorer/js/controllers.js
@@ -5,41 +5,120 @@
 assetApp.controller('AssetListCtrl', function ($scope, assetService) {
     $scope.searchTerm = "";
     $scope.assetType = "text";
-    $scope.assetReturn = "10";
+    $scope.pageSize = "10";
     $scope.filterTerm = "";
     $scope.sortProp = "";
     $scope.showFlag = false;
-    $scope.searchType = "1";
+    $scope.searchTarget = "1";
     $scope.propertyType = "6";
     $scope.siteType = "6";
     $scope.agType = "6";
     $scope.section = "";
     $scope.status = "Unpublished";
+    $scope.curPage = 0;
+    $scope.startpage = 1;
 
     $scope.btnSearchDisabled = true;
 
-    
+    $scope.numberOfItems = function () {
+        return (0);
+    };
 
     $scope.doSearch = function () {
         var type = $scope.assetType;
         var statusname = $scope.statusname;
-        var numrows = $scope.assetReturn;
+        var numrows = $scope.pageSize;
+        var starg = $scope.searchTarget;
+       
+       // $scope.pageSize = $scope.assetReturn;
+
         if ($scope.assetType === "all") type = "";
         assetService.get({
-            keyword: $scope.searchTerm, assettypename: type, propertyid: 1, rows: numrows, statusid: 1, sc: 229, apiKey: 'special-key', api_key: 'eywmgxp93u4cm6b85aa6s4td'
+            keyword: $scope.searchTerm, assettypename: type, propertyid: starg, rows: numrows, statusid: 1, sc: 229, apiKey: 'special-key', api_key: 'eywmgxp93u4cm6b85aa6s4td'
         }, function (response) {
+            $scope.assettotResults = response.totalNumResults;
             $scope.assetResults = response.results;
         });
+
+        $scope.numberOfItems = function () {
+            return ($scope.assettotResults);
+        };
+        $scope.numberOfPages = function () {
+           return Math.ceil($scope.assettotResults / $scope.pageSize);
+        };
     };
 
-    /*Deploy change the route here*/
+    $scope.searchUp = function () {
+        var sterm = $scope.searchTerm;
+        var type = $scope.assetType;
+        var numrows = $scope.pageSize;
+        var starg = $scope.searchTarget;
+        var cpage = $scope.startpage;
+
+        
+        var spage = (Number(cpage) * Number(numrows)) + 1;
+
+        if ($scope.assetType === "all") type = "";
+
+        //alert("search term: " + sterm + ", search asset type: " + type + ", start row: " + spage + ", numrows: " + numrows + ", target: " + starg);
+       
+        assetService.get({
+            keyword: $scope.searchTerm, assettypename: type, start: spage, propertyid: starg, rows: numrows, statusid: 1, sc: 229, apiKey: 'special-key', api_key: 'eywmgxp93u4cm6b85aa6s4td'
+        }, function (response) {
+            $scope.assettotResults = response.totalNumResults;
+            $scope.assetResults = response.results;
+        });
+      
+        $scope.curPage++;
+        $scope.startpage++;
+
+        
+    };
+
+    $scope.searchDwn = function () {
+        var sterm = $scope.searchTerm;
+        var type = $scope.assetType;
+        var numrows = $scope.pageSize;
+        var starg = $scope.searchTarget;
+        var cpage = $scope.curPage--;
+
+
+
+        var spage = (Number(cpage) * Number(numrows)) + 1;
+
+        if ($scope.assetType === "all") type = "";
+
+        //alert("search term: " + sterm + ", search asset type: " + type + ", start row: " + spage + ", numrows: " + numrows + ", target: " + starg);
+
+        assetService.get({
+            keyword: $scope.searchTerm, assettypename: type, start: spage, propertyid: starg, rows: numrows, statusid: 1, sc: 229, apiKey: 'special-key', api_key: 'eywmgxp93u4cm6b85aa6s4td'
+        }, function (response) {
+            $scope.assettotResults = response.totalNumResults;
+            $scope.assetResults = response.results;
+        });
+
+        //$scope.curPage--;
+        $scope.startpage--;
+
+
+    };
+
+    /*prod*/
+    //$scope.UpLoad = function () {
+
+    //    var ct = document.getElementById('list').value;
+    //    location.href = "http://54.84.143.40/ingest_prod/Handler.ashx?source=Search" + "&vertical=" + $scope.section + "&status=" + $scope.status + "&list=" + ct;
+
+    //};
+    
+    /*dev*/
     $scope.UpLoad = function () {
 
         var ct = document.getElementById('list').value;
-        location.href = "http://54.84.143.40/ingest_prod/Handler.ashx?source=Search" + "&vertical=" + $scope.section + "&status=" + $scope.status + "&list=" + ct;
+        location.href = "http://localhost:53198/Handler.ashx?source=Search" + "&vertical=" + $scope.section + "&status=" + $scope.status + "&list=" + ct;
 
     };
-    
+
     $scope.remaining = function () {
         var count = 0;
         angular.forEach($scope.assetResults, function (item) {
@@ -48,6 +127,8 @@ assetApp.controller('AssetListCtrl', function ($scope, assetService) {
         });
         return count;
     };
+
+    
 
     $scope.listtogaia = function () {
         var count = "";
